56da9a6739417c54fe2ebb3ea6e33425285d08c7
Chandan <chaandan1979@gmail.com>
2017-03-03 01:06:24 +0530

    First commit test

---
+++ src/main/java/com/nerdster/FirstChandanProjectApplication.java
@@ -0,0 +1,12 @@
+package com.nerdster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstChandanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstChandanProjectApplication.class, args);
+	}
+}
